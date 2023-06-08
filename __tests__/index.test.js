bfec9fa46396e4dc73b70f8a17735af845283ea8
kurbanovaIrina <kurbanovaim@gmail.com>
2023-06-08 04:37:55 +0300

    add additional tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,8 +7,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 
-test('genDiff', () => {
+test('genDiffJSONFile', () => {
   const current = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   const expected = readFileSync(getFixturePath('expectedFile.txt'), 'utf-8');
   expect(current).toEqual(expected);
+});
+
+test('genDiffYmlFile', () => {
+  const current = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'));
+  const expected = readFileSync(getFixturePath('expectedFile.txt'), 'utf-8');
+  expect(current).toEqual(expected);
+});
+
+test('genDiffUnexpectedFile', () => {
+  expect(() => genDiff(getFixturePath('errorFile.txt'), getFixturePath('file2.yml'))).toThrow(Error);
 });
--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@
 
 import fs from 'fs';
 import path from 'path';
-import parse from './parse.js';
+import parse from './parsers.js';
 import compare from './compare.js';
 
 const getPathFile = (filePath) => path.resolve(filePath);
 const getTypeOfFile = (filePath) => path.extname(filePath).slice(1);
+const getDataFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 const genDiff = (path1, path2) => {
   const pathFile1 = getPathFile(path1);
   const pathFile2 = getPathFile(path2);
-  const getDataFile1 = fs.readFileSync(pathFile1);
-  const getDataFile2 = fs.readFileSync(pathFile2);
+  const getDataFile1 = getDataFile(pathFile1);
+  const getDataFile2 = getDataFile(pathFile2);
   const parsedFile1 = parse(getDataFile1, getTypeOfFile(path1));
   const parsedFile2 = parse(getDataFile2, getTypeOfFile(path2));
   const result = compare(parsedFile1, parsedFile2);
